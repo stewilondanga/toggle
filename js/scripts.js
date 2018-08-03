ff79bd2dadc2f43f8395ff6ad5036c3577e32d50
Stewart ilondanga <stilondanga@gmail.com>
2018-08-03 16:08:36 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+// JUST FOR BACKGROUND CHANGE
+const toggle = document.querySelector('#toggle');
+
+const updateBackground = (event) => {
+document.body.classList.toggle('on');
+}
+
+toggle.addEventListener("click", () => document.body.classList.toggle('on') , false);
